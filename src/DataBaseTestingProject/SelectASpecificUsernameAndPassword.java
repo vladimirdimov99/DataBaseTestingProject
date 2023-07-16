7b93a7e7cfa535a0580e1a764295285bdcf024b8
Vladimir <vladimirdimov99@gmail.com>
2023-07-16 19:54:53 +0300

    Add new Class for Selecting a specific Username and Password

--- src/DataBaseTestingProject/SelectASpecificUsernameAndPassword.java
+++ src/DataBaseTestingProject/SelectASpecificUsernameAndPassword.java
@@ -1,5 +1,6 @@
 package DataBaseTestingProject;
 
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -8,7 +9,8 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
 
-public class InsertDataAndReadTheDatabase {
+public class SelectASpecificUsernameAndPassword {
+    // Connection object
     static Connection con = null;
     // Statement object
     private static Statement stmt;
@@ -35,17 +37,7 @@ public class InsertDataAndReadTheDatabase {
     }
 
     @Test(priority = 1)
-    public void InsertDataToTheTable() {
-
-        try {
-            String query = "INSERT INTO credentials VALUES ('vladimir99', 'test123')";
-            stmt.executeUpdate(query);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-    }
-
-    @Test(priority = 2)
+    //First Read the DataBase so we can see which exact Username and Password we want to SELECT
     public void ReadTheDataBase() {
         try {
             String query = "SELECT * FROM credentials";
@@ -58,9 +50,35 @@ public class InsertDataAndReadTheDatabase {
                 System.out.print(" " + res.getString(2));
                 System.out.println();
             }
-            //The Data is successfully inserted into the DataBase
         } catch (Exception e) {
             e.printStackTrace();
+        }
+    }
+
+    @Test(priority = 2)
+    public void SelectASpecificUsernameAndPasswordThenPrintIt(){
+        try {
+            String query = "SELECT `Username`, `Password` FROM `credentials` WHERE Username = 'vladimir99' && Password = 'test123'";
+            // Get the contents of userinfo table from DB
+            ResultSet res = stmt.executeQuery(query);
+            // Print the result until all the records are printed
+            // res.next() returns true if there is any next record else returns false
+            while (res.next()) {
+                System.out.print(res.getString(1));
+                System.out.print(" " + res.getString(2));
+                System.out.println();
+            }
+            // The specific Username and Password is successfully SELECTED and then printed
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    @AfterTest
+    public void tearDown() throws Exception {
+        // Close DB connection
+        if (con != null) {
+            con.close();
         }
     }
 }
